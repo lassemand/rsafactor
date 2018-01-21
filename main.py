16c426ab29d05787328e215807470e824b68b7df
Lasse Alm <lassealm2@gmail.com>
2018-01-21 21:07:42 +0100

    added persistance

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from generator import generate_factorizers_dict
+from persistance import sqlite_persistance
 from statistics import average_of_factorizers
 
 if __name__ == "__main__":
-    factorizer_dict = generate_factorizers_dict([32, 64])
-    result = average_of_factorizers(factorizer_dict, 1)
-
-    print("test")
+    factorizer_dict = generate_factorizers_dict([32])
+    result = average_of_factorizers(factorizer_dict, 2)
+    persistance = sqlite_persistance()
+    persistance.save_statistics(result, "brute_force")
--- persistance.py
+++ persistance.py
@@ -5,11 +5,20 @@ from sqlite3 import Error
 class sqlite_persistance:
     def __init__(self):
         try:
-            conn = sqlite3.connect("rsafactor.db")
-            print(sqlite3.version)
+            self.conn = sqlite3.connect("rsafactor.db")
+            c = self.conn.cursor()
+            c.execute("CREATE TABLE IF NOT EXISTS statistics (method TEXT NOT NULL, bits INT NOT NULL, time BIGINT NOT NULL)")
+            self.conn.commit()
         except Error as e:
             print(e)
-        finally:
-            conn.close()
+
+    def save_statistics(self, results, method):
+        c = self.conn.cursor()
+        c.executemany("INSERT INTO statistics VALUES (?, ?, ?)", [(method, key, item) for key, value in results.items() for item in value])
+        self.conn.commit()
+
+    def close_connection(self):
+        self.conn.close()
+
 
 
