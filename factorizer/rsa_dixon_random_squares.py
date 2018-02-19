2322072f0e97e95c334267db4c6d9355f31fcdb8
Lasse Alm <lassealm2@gmail.com>
2018-02-19 13:05:23 +0100

    reformatted code and added intersection

--- factorizer/rsa_dixon_random_squares.py
+++ factorizer/rsa_dixon_random_squares.py
@@ -30,6 +30,22 @@ def factorize_number_from_primes(number, primes, n):
     return None, None
 
 
+def find_next_selection(self, row_ones, pointers):
+    temp_pointers = list(pointers)
+    i = 0
+    while i != len(pointers):
+        is_in_the_begining_and_should_update = i == 0 and pointers[i] == len(row_ones) - 1
+        is_not_in_the_begining_and_should_update = pointers[i - 1] == pointers[i] + 1
+        if is_in_the_begining_and_should_update or is_not_in_the_begining_and_should_update:
+            i += 1
+            continue
+        pointers[i] += 1
+        for index in reversed(range(i)):
+            pointers[index] = pointers[index + 1] + 1
+        return temp_pointers
+    return None
+
+
 class rsa_dixon_random_squares(implements(rsa_factorizer)):
     def __init__(self, n, e):
         self.n = n
@@ -40,88 +56,61 @@ class rsa_dixon_random_squares(implements(rsa_factorizer)):
         self.all_rows_in_factor = [None] * (len(self.B) + 4)
         self.all_rows_in_binary_factor = [None] * (len(self.B) + 4)
 
-    def test_congruences(self, forced_ones, Z, all_rows_in_factor, n, B):
+    def test_congruences(self, forced_ones):
         list_z_values = list(forced_ones)
-        z_congruence = np.prod(Z[list_z_values]) % n
-        y_values = B[1:] ** (np.sum(all_rows_in_factor[:, 1:][list_z_values, :], axis=0) / 2)
-        y_congruence = int(np.prod(y_values)) % n
-        p = math.gcd(z_congruence + y_congruence, n)
-        if p != 1 and p != n:
-            return p, int(n / p)
+        z_congruence = np.prod(self.Z[list_z_values]) % self.n
+        y_values = self.B[1:] ** (np.sum(self.all_rows_in_factor[:, 1:][list_z_values, :], axis=0) / 2)
+        y_congruence = int(np.prod(y_values)) % self.n
+        p = math.gcd(z_congruence + y_congruence, self.n)
+        if p != 1 and p != self.n:
+            return p, int(self.n / p)
         return None, None
 
-    def find_factor_from_all_possibilites(self, d, row_ones, ones, Z, all_rows_in_factor, B, n, current_index,
-                                          forced_zeros,
-                                          forced_ones):
-        pointers = [i for i in reversed(range(d))]
-        while i != len(pointers):
-            is_in_the_begining_and_should_update = i == 0 and pointers[i] == len(row_ones) - 1
-            is_not_in_the_begining_and_should_update = pointers[i - 1] == pointers[i] + 1
-            if is_in_the_begining_and_should_update or is_not_in_the_begining_and_should_update:
-                i += 1
-                continue
-            pointers[i] += 1
-            for index in reversed(range(i)):
-                pointers[index] = pointers[index + 1] + 1
-            current_selection = row_ones[pointers]
-            p, q = self.factor_from_reduced_matrix(ones, Z, all_rows_in_factor, B, n, current_index + 1,
-                                                   forced_zeros + row_ones[np.setdiff1d(row_ones, current_selection)],
-                                                   forced_ones + current_selection)
-            if p is not None and q is not None:
-                return p, q
-            i = 0
-        return None, None
-
-    def factor_from_reduced_matrix(self, ones, Z, all_rows_in_factor, B, n, current_index=0, forced_zeros=set(),
-                                   forced_ones=set()):
+    def factor_from_reduced_matrix(self, ones, current_index=0, forced_zeros=set(), forced_ones=set()):
         if current_index == len(ones):
-            return self.test_congruences(forced_ones, Z, all_rows_in_factor, n, B)
+            return self.test_congruences(forced_ones)
         row_ones = ones[current_index]
-
         ones_disjoint = [one for one in row_ones if one not in forced_ones and one not in forced_zeros]
-        for d in reversed(range(0, len(ones_disjoint) + 1, 2)):
-            p, q = self.factor_from_reduced_matrix(ones, Z, all_rows_in_factor, B, n, current_index + 1,
-                                                   forced_zeros.union(set(ones_disjoint[d:])),
-                                                   forced_ones.union(set(ones_disjoint[:d])))
-            if p is not None and q is not None:
-                return p, q
-            p, q = self.find_factor_from_all_possibilites(d, row_ones, Z, all_rows_in_factor, B, n, current_index,
-                                                          forced_zeros,
-                                                          forced_ones)
-            if p is not None and q is not None:
-                return p, q
+        ones_intersect = forced_ones.intersection(row_ones)
+        for d in reversed(range(0, len(ones_disjoint) + len(ones_intersect) + 1, 2)):
+            pointers = [i for i in reversed(range(d - len(ones_intersect)))]
+            current_selection = find_next_selection(row_ones, pointers)
+            while current_selection is not None:
+                p, q = self.factor_from_reduced_matrix(ones, current_index + 1,
+                                                       forced_zeros + row_ones[
+                                                           np.setdiff1d(row_ones, current_selection)],
+                                                       forced_ones + current_selection)
+                if p is not None and q is not None:
+                    return p, q
+                current_selection = find_next_selection(row_ones, pointers)
         return None, None
 
-    def dixon(self, c=0):
+    def factorize(self, c=0):
         j = 0
         i = 0
         while i < len(self.B) + 4:
             j = j + 1
             if j & 1:
-                Z[i] = math.floor(math.sqrt(((j + 1) / 2) * self.n)) - c
+                self.Z[i] = math.floor(math.sqrt(((j + 1) / 2) * self.n)) - c
                 should_negate_z_value = [1]
             else:
-                Z[i] = math.ceil(math.sqrt((j / 2) * self.n)) + c
+                self.Z[i] = math.ceil(math.sqrt((j / 2) * self.n)) + c
                 should_negate_z_value = [0]
-            number = Z[i] ** 2 % self.n
+            number = self.Z[i] ** 2 % self.n
             if should_negate_z_value[0]:
                 number = (number * -1) % self.n
-            factorized_binary_number_row, factorized_number_row = factorize_number_from_primes(number, B, self.n)
+            factorized_binary_number_row, factorized_number_row = factorize_number_from_primes(number, self.B, self.n)
             if factorized_number_row is not None:
-                all_rows_in_binary_factor[i] = should_negate_z_value + factorized_binary_number_row
-                all_rows_in_factor[i] = should_negate_z_value + factorized_number_row
+                self.all_rows_in_binary_factor[i] = should_negate_z_value + factorized_binary_number_row
+                self.all_rows_in_factor[i] = should_negate_z_value + factorized_number_row
                 i = i + 1
-        all_rows_in_binary_factor, all_rows_in_factor = np.array(all_rows_in_binary_factor), np.array(
-            all_rows_in_factor)
-        matrix, numpivots = reduced_row_echelon_form(all_rows_in_binary_factor.transpose())
-        # I add this element now because it makes the factoring easier not to have it from the beginning
-        B = np.insert(B, 0, -1)
+        self.all_rows_in_binary_factor, self.all_rows_in_factor = np.array(self.all_rows_in_binary_factor), np.array(
+            self.all_rows_in_factor)
+        matrix, numpivots = reduced_row_echelon_form(self.all_rows_in_binary_factor.transpose())
+        self.B = np.insert(self.B, 0, -1)
         ones = np.array(
             [[index for (index, bit) in enumerate(matrix[i, :]) if bit] for i in reversed(range(numpivots))])
-        p, q = factor_from_reduced_matrix(ones, Z, all_rows_in_factor, B, self.n)
+        p, q = self.factor_from_reduced_matrix(ones)
         if p is not None and q is not None:
             return p, q
-        return self.dixon(B, c + 1)
-
-    def factorize(self):
-        return self.dixon()
+        return self.factorize(c + 1)
--- test/factorizers/rsa_dixon_random_square_test.py
+++ test/factorizers/rsa_dixon_random_square_test.py
@@ -6,13 +6,18 @@ from factorizer.rsa_dixon_random_squares import rsa_dixon_random_squares
 
 class rsa_dixon_random_square_test(unittest.TestCase):
 
+    def test_find_next_selection(self):
+        selection = find_next_selection([[2, 3, 4], [0, 1]])
+        self.assertEqual(p, 7)
+        self.assertEqual(q, 11)
+
     def test_dixon_random_squares_with_book_example(self):
         sut = rsa_dixon_random_squares(77, 1)
         (p, q) = sut.factorize()
-        if p == 7:
-            p, q = 3, 7
-        self.assertEqual(p, 3)
-        self.assertEqual(q, 7)
+        if p == 11:
+            p, q = 7, 11
+        self.assertEqual(p, 7)
+        self.assertEqual(q, 11)
 
 if __name__ == '__main__':
     unittest.main()
