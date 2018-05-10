ca1d6a6c7ac6cafeb7501b5001f6d1d532188e3a
Lasse Alm <lassealm2@gmail.com>
2018-05-10 13:11:23 +0200

    updated independent approach

--- factorizer/pollard_rho/rsa_pollard_rho_parallel_independent.py
+++ factorizer/pollard_rho/rsa_pollard_rho_parallel_independent.py
@@ -1,17 +1,7 @@
-import random
-from multiprocessing import Process
-from multiprocessing import Queue
-
 from interface import implements
 
 from factorizer.pollard_rho.rsa_pollard_rho import rsa_pollard_rho
 from factorizer.rsa_factorizer import rsa_factorizer
-
-
-def worker(n, e, queue, i):
-    fac = rsa_pollard_rho(n, e, i)
-    a = random.randint(1, 101)
-    queue.put(fac.factorize(f = lambda u: u ** 2 + a))
 
 
 class rsa_pollard_rho_parallel_independent(implements(rsa_factorizer)):
@@ -20,12 +10,7 @@ class rsa_pollard_rho_parallel_independent(implements(rsa_factorizer)):
         self.e = e
 
     def factorize(self):
-        queue = Queue()
-        process = [Process(target=worker, args=(self.n, self.e, queue, i,)) for i in range(3)]
-        for t in process:
-            t.start()
-        p, q = queue.get()
-        for t in process:
-            t.terminate()
+        fac = rsa_pollard_rho(self.n, self.e)
+        p, q = fac.factorize()
         return p, q
 
